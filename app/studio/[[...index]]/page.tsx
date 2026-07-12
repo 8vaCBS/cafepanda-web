191ed938a9b00b5a250949fa3aa86fe46739c72b
8vaCBS <andres.lruiz@gmail.com>
2026-07-12 00:16:11 -0400

    fix: Usar Sanity Studio hosted en iframe

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -1,15 +1,11 @@
-import dynamic from 'next/dynamic'
-import { Suspense } from 'react'
-
-const StudioComponent = dynamic(() => import('@/lib/studio'), {
-  ssr: false,
-  loading: () => <div className="p-8 text-center">Cargando Studio...</div>,
-})
-
 export default function StudioPage() {
   return (
-    <Suspense fallback={<div className="p-8 text-center">Cargando...</div>}>
-      <StudioComponent />
-    </Suspense>
+    <div className="w-full h-screen">
+      <iframe
+        src="https://m8varzzi.sanity.studio"
+        className="w-full h-full border-none"
+        title="Sanity Studio"
+      />
+    </div>
   )
 }
